a774b6557acdaabd3d6a9934a01af8a4116599e8
Nusrat555 <nusratjahansara750@gmail.com>
2024-02-20 14:31:54 +0600

    first commit

---
+++ Nested.js
@@ -0,0 +1,13 @@
+var Kitketprice = 80;
+var chokletprice = 40;
+var chipsprice = 20;
+var myBudget = 60;
+
+if(Kitketprice < myBudget){
+    console.log("Bikale ami Ghurte jai")
+}
+else if(chokletprice < myBudget){
+    console.log("bikale ami ghurte jaiya choklet khabo")
+}else if(chipsprice < myBudget){
+    console.log("ghurte gele choklet khabo")
+}
---
+++ if, else.js
@@ -0,0 +1,12 @@
+var gotjob = false;
+var saveMoney = 300000;
+var hasflat = true;
+var hasHouse = true;
+
+
+if((gitJob == true || saveMoney > 200000) & (hasflat == true|| hasHouse == true)){
+    console.log ('cholo biye kori!!');
+}
+else{
+    console.log('tur kopale biye nai!!');
+}
